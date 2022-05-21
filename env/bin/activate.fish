e92267dacf46552b42c9376a19950e34b45819af
sbittu348@gmail.com <kishan.sharma@atpl.live>
2022-05-21 09:19:06 +0530

    my first commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kishan/kishan/kp/env"
+set -gx VIRTUAL_ENV "/home/kishan/kishan/ultasidha/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
